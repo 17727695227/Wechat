032c56b4e9d08d28088efece4fe6c8fd30617c0a
17727695227 <13124937709@163.com>
2017-08-02 15:49:48 +0800

    2

---
+++ wechat.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : zhiwei
+Source Server Version : 50717
+Source Host           : localhost:3306
+Source Database       : wechat
+
+Target Server Type    : MYSQL
+Target Server Version : 50717
+File Encoding         : 65001
+
+Date: 2017-08-02 15:48:01
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
